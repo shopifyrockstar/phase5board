78439677059bfbe059b310c70f27f2bd2a0d8700
Shopify Rockstar <shopifyrockstar@outlook.com>
2022-09-26 06:44:25 -0700

    update 2023 product template

--- assets/theme.js
+++ assets/theme.js
@@ -2816,17 +2816,59 @@ $('.additional-slider-wrapper').slick({
   slidesToScroll: 1
 });
 
-$(document).ready(function(){
-    if ( $(window).width() < 750 ){    	
-      $('.three-image-inner-wrapper').slick({
-        infinite: true,
+
+$('.product-slider-wrapper .three-image-inner-wrapper').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1
-      });
-      $('.two-image-inner-wrapper').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      });
+      }
     }
-  })
+  ]
+});
+
+$('.product-slider-wrapper .two-image-inner-wrapper').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$(document).ready(function(){
+  // if ( $(window).width() > 750 ){
+    
+  // }
+  if ( $(window).width() < 750 ){    	
+    $('.three-image-inner-wrapper').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+    $('.two-image-inner-wrapper').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  }
+})
